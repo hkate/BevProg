a6cfac82686c79288dad1de7c7525a41be296267
Kata Halász <halasz.kata01@gmail.com>
2021-09-11 19:18:02 +0200

    first commit

---
+++ week02/input.cpp
@@ -0,0 +1,14 @@
+#include ""
+
+int main() {
+
+    cout << "Please enter your first name:\n";
+    
+    string first_name;
+
+    cin >> first_name;
+
+    cout << "Hello, " << first_name << "\n"
+
+    return 0;    
+}
