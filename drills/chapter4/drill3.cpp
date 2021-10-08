50b06af581634a9e783a8fde7643fa00d2884e68
Kata Halász <halasz.kata01@gmail.com>
2021-10-08 20:08:52 +0200

    drill 3, part 9-11

--- drills/chapter4/drill3.cpp
+++ drills/chapter4/drill3.cpp
@@ -44,32 +44,58 @@ int main() {
     const double cm_per_meter = 100;
     const double inch_per_ft = 12;
     char accepted = 'n';
+    double sum = 0;
+    double values = 0;
+    vector<double> allvalues;
 
-    cout << "Enter a number."<< endl;
 
-    while (cin >> number >> unit){
-        cout << number << endl;
-        for(int i = 0; i < units.size(); i++){
-            if(unit == units[i]) {
-                accepted = 'i';
+    cout << "Enter a number. If you finished press Ctrl-D."<< endl;
+
+        while (cin >> number >> unit){
+
+            if(unit == "cm")
+                number = number/cm_per_meter;
+            else if(unit == "in")
+                number = (number*cm_per_inch)/cm_per_meter;
+            else if(unit == "ft")
+                number = ((number*inch_per_ft)*cm_per_inch)/cm_per_meter;
+
+    allvalues.push_back(number);
+
+            cout << number << endl;
+                for(int i = 0; i < units.size(); i++){
+                    if(unit == units[i]) {
+                        accepted = 'i';
+                    }
                 }
-                
-        }
-        if(accepted == 'n') {
-        cout << "Illegal unit!" << endl;
-            abort();
-        }
+                    if(accepted == 'n') {
+                        cout << "Illegal unit!" << endl;
+                    abort();
+                    }
 
         accepted = 'n';
 
         if (number < smallest) {
-        smallest = number;
-        cout << "is the smallest so far." << endl;
+            smallest = number;
+            cout << "is the smallest so far." << endl;
         }
         if (number > largest) {
-        largest = number;
-        cout << "is the largest so far." << endl;
+            largest = number;
+            cout << "is the largest so far." << endl;
         }
 
+        values = values + 1;
+        sum = sum + number;
+
+    }
+    sort(allvalues);
+        cout << smallest << "m was the smallest." << endl;
+        cout << largest << "m was the largest." << endl;
+        cout << values << " is the number of values." << endl;
+        cout << sum << "m is the sum of values." << endl;
+        cout << "These were the values:" << endl;
+
+    for(auto av : allvalues){
+        cout << av << endl;
     }
 }
